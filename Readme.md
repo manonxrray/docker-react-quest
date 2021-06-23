7e9e05df5d7b2896ab341b28e9ac93b0ba59743b
wazo <manon.gerray@gmail.com>
2021-06-23 11:04:01 +0200

    :whale2: Docker & React

---
+++ Readme.md
@@ -0,0 +1,7 @@
+#Discovering & Using Docker with a React app !
+
+
+    docker-compose -f docker-compose.dev.yml up --build
+
+
+> Remerciements : merci à famille de m'avoir élevée, d'avoir toujours été à mes côtés et de m'avoir soutenue dans les bons comme dans les mauvais moments. Merci à mes amis : Fabrice, Estelle, Marie, Paul, Tristan, Capucine, Marc, Francis, William, Mathilde, Tanguy, Pascal, Jean-Killian... et je vais en oublier certains. Merci également (1er degré cette fois) à mes camarades patients : Alicia et Quentin. JE SUIS HEUREUSE D'AVOIR FINI CETTE QUÊTE
---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Is my React app working ?
+        </p>
+        <p>
+          ...YES !!!
+        </p>
+        <p>
+          (c'est le plus beau jour de ma vie, ma vie est nulle oui)
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
